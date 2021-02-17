4d7297384a14e26f54a5bb62b55eee34858d476e
katinda <katindamangala@gmail.com>
2021-02-17 14:16:01 +0100

    js

---
+++ exo2.js
@@ -0,0 +1,19 @@
+/*var meche;// pour la métaphore... allumer/éteindre la mèche ^^
+function click_simple() {
+   if (!meche)// pour ne pas lancer la fonction en doublon/triplon/etc.
+      meche = setTimeout(function() {
+         alert("simple clic");
+      }, 500);// une demi-seconde par exemple mais à ajuster probablement
+   }
+
+ 
+function click_double() {
+   if (meche) clearTimeout(meche);// pschit.
+   alert("double clic");
+} */
+
+
+function click_simple(){
+    
+}
+
